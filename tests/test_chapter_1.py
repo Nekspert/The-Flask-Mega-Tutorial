54d7ac62163dc6fed97d8bc375f19e363ead5ec3
Timur Berezin <corporate.timur01@gmail.com>
2025-01-19 16:26:50 +0400

    test: chapter1: assert response.status_code == 200

--- tests/test_chapter_1.py
+++ tests/test_chapter_1.py
@@ -3,6 +3,7 @@ from conftest import client
 
 def test_request_hello_world(client):
     response = client.get("/")
+    assert response.status_code == 200
     assert b"<h1>Hello World</h1>" in response.data
 
 
