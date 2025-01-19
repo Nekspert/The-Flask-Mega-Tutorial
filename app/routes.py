504a0040301f118bc9b94f7ac30b6aeeed92de7f
Timur Berezin <corporate.timur01@gmail.com>
2025-01-19 17:59:59 +0400

    Chapter 2: Templates
    Used: conditional statements, loops, template inheritance
    
    Create base.html that all templates inheried them.
    
    Create index.html like a "home page".

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,23 @@
+from flask import render_template
+
 from app import app
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "<h1>Hello World</h1>"
+    user = {'username': 'Timur'}
+    posts = [
+        {
+            "author": {"username": "John"},
+            "body": "Beautiful day in Portland!"
+        },
+        {
+            "author": {"username": "Rasul"},
+            "body": "The avengers movie was so cool!"
+        }
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
+
+
+
