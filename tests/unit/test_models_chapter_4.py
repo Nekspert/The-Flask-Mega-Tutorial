501c1f9b7b1a0db1f7c6d36f0d46936b9d9e64c1
Timur Berezin <corporate.timur01@gmail.com>
2025-01-23 21:13:55 +0400

    test: chapter 4 add tests for fourth chapter(database)

---
+++ tests/unit/test_models_chapter_4.py
@@ -0,0 +1,16 @@
+from app.models import Post
+
+
+def test_new_user(user):
+    assert user.username == "user"
+    assert user.email == "email@example.com"
+    assert user.id is None
+    assert user.password_hash is None
+
+
+def test_new_post(user):
+    post = Post(body="Hello my name is user!", author=user)
+    assert post.body == "Hello my name is user!"
+    assert post.author == user
+    assert post.id is None
+    assert post.user_id is None
