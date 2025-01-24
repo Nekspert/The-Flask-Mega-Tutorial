853a2cbd83192208420fe8aadb57a8a3654b18c7
Timur Berezin <corporate.timur01@gmail.com>
2025-01-24 18:22:50 +0400

    test: chapter 5:
    fix functional tests for chapter 2
    fix functional tests for chapter 3
    
    add functional tests for chapter 5
    add unit tests for chapter 5

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,12 @@
+import os
+
 import pytest
-from microblog import app
+
+from app import app, db
 from app.models import User
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="session")
 def conf_app():
     app.config.update(
         {"TESTING": True}
@@ -15,9 +18,19 @@ def conf_app():
     yield app
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="session")
 def client(conf_app):
-    return conf_app.test_client()
+    with conf_app.app_context():
+        db.create_all()
+
+        yield conf_app.test_client()
+
+        db.session.remove()
+        db.drop_all()
+        db.engine.dispose()
+
+    if os.path.exists(conf_app.config["SQLALCHEMY_DATABASE_URI"].split("///")[1]):
+        os.remove(conf_app.config["SQLALCHEMY_DATABASE_URI"].rsplit("///")[1])
 
 
 @pytest.fixture(scope="module")
--- tests/functional/test_chapter_2.py
+++ tests/functional/test_chapter_2.py
@@ -2,13 +2,9 @@ def test_request_index(client):
     response = client.get("/")
     assert response.status_code == 200
     assert b"Microblog" in response.data
-    assert b"Timur" in response.data
-    assert b"Rasul says: " in response.data
-    assert b"The avengers movie was so cool!" in response.data
+    assert b"Hi," in response.data
 
     response = client.get("/index")
     assert response.status_code == 200
     assert b"Microblog" in response.data
-    assert b"Timur" in response.data
-    assert b"Rasul says: " in response.data
-    assert b"The avengers movie was so cool!" in response.data
+    assert b"Hi," in response.data
