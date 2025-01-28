ad1497c1f354e6a801f6c01b9abed022cd5a177e
Timur Berezin <corporate.timur01@gmail.com>
2025-01-28 11:46:58 +0400

    Chapter 9: Pagination
    Add: Explore page for view all posts on application (with pagination)
    
    Add: PostForm for applying(submitting) post
    
    Add: Create posts functional in index.html page
    
    Add: Pagination for posts (PER 25)
    
    Add: view posts in index.html (index endpoint) with pagination
    
    Add: view posts in user.html (user endpoint) with pagination

--- app/forms.py
+++ app/forms.py
@@ -57,3 +57,8 @@ class EditProfileForm(FlaskForm):
 
 class EmptyForm(FlaskForm):
     submit = SubmitField("Submit")
+
+
+class PostForm(FlaskForm):
+    post = TextAreaField("Say something", validators=[DataRequired(), Length(min=1, max=256)])
+    submit = SubmitField("Submit")
