598364e04e1aa8ec9b8618ed2c5161b2a07abd3a
Timur Berezin <corporate.timur01@gmail.com>
2025-01-28 21:56:13 +0400

    Chapter 10: Email Support
    Add: reset password functional by using email sending
    
    Add: ResetPasswordRequestForm for entering email address for sending and ResetPasswordForm for applying reset password
    
    Add: get_reset_password_token() and verify_reset_password_token() for generating Json Web Token(jwt) and checking this token
    
    Add: 2 endpoints(view functions) for processing reset password functional

--- app/models.py
+++ app/models.py
@@ -1,13 +1,15 @@
 from typing import Optional
+from time import time
 from datetime import datetime, timezone
 
+import jwt
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
 from werkzeug.security import generate_password_hash, check_password_hash
 from hashlib import md5
 from flask_login import UserMixin
 
-from app import db, login
+from app import db, login, app
 
 followers = sa.Table(
     "followers",
@@ -89,6 +91,21 @@ class User(UserMixin, db.Model):
             .group_by(Post)
             .order_by(Post.timestamp.desc())
         )
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {"reset_password": self.id, "exp": time() + expires_in},
+            app.config["SECRET_KEY"], algorithm="HS256"
+        )
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config["SECRET_KEY"],
+                            algorithms="HS256")["reset_password"]
+        except:
+            return
+        return db.session.get(User, id)
 
     def __repr__(self):
         return f"<User {self.username}>"
